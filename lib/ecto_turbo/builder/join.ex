55bd55133bc7438552e6e94d5f59c5b80ad41ef4
docJerem <jeremie.flandrin@gmail.com>
2026-03-26 15:01:07 +0100

    🔒 Remove Code.eval_quoted from Join builder (Phase 4)
    Replace internal Ecto.Query.Builder.Join API with public
    Ecto.Query.join/5 macro using pinned association names.

--- lib/ecto_turbo/builder/join.ex
+++ lib/ecto_turbo/builder/join.ex
@@ -1,10 +1,10 @@
 defmodule EctoTurbo.Builder.Join do
   @moduledoc false
 
-  alias Ecto.Query.Builder.Join, as: Join
+  import Ecto.Query
 
   @doc """
-  Builds a quoted join expression.
+  Builds a join expression.
 
   ## Example
 
@@ -14,34 +14,13 @@ defmodule EctoTurbo.Builder.Join do
       #Ecto.Query<from p0 in EctoTurbo.Schemas.Post, join: c1 in assoc(p0, :category), join: r2 in assoc(p0, :repleis)>
 
   """
-  @spec build(Ecto.Query.t(), [atom()]) :: Ecto.Query.t()
+  @spec build(Ecto.Queryable.t(), [atom()]) :: Ecto.Queryable.t()
   def build(query, relations) do
-    relations |> Enum.reduce(query, &apply_join(&1, &2))
+    Enum.reduce(relations, query, &apply_join(&1, &2))
   end
 
   @spec apply_join(atom(), Ecto.Queryable.t()) :: Ecto.Query.t()
   def apply_join(relation, query) do
-    query
-    |> Macro.escape()
-    |> Join.build(
-      :inner,
-      [{:query, [], Elixir}],
-      expr(relation),
-      nil,
-      nil,
-      nil,
-      nil,
-      nil,
-      __ENV__
-    )
-    |> elem(0)
-    |> Code.eval_quoted()
-    |> elem(0)
-  end
-
-  defp expr(relation) do
-    quote do
-      unquote(Macro.var(relation, Elixir)) in assoc(query, unquote(relation))
-    end
+    join(query, :inner, [q], x in assoc(q, ^relation))
   end
 end
